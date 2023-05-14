37911a2c3e5479a3ae26c476b0034979b04cc5d3
Anastasiia Mal <nastymal2003@gmail.com>
2023-05-14 15:48:09 +0200

    Finished lab 3

---
+++ src/Controller/UniversityCreator.java
@@ -0,0 +1,32 @@
+package Controller;
+
+import Model.Faculty;
+import Model.Human;
+import Model.University;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityCreator {
+    public University createUniversity(String name, int countOfFaculties, Human rector) {
+        University university = new University();
+        FacultyCreator facultyCreator = new FacultyCreator();
+        university.setName(name);
+        university.setHead(rector);
+        List<Faculty> faculties = new ArrayList<>();
+        for (int i = 1; i < countOfFaculties; i++) {
+            faculties.add(facultyCreator.createTypicalFaculty());
+        }
+        university.setFaculties(faculties);
+
+        return university;
+
+    }
+
+    public  University createTypicalUniversity() {
+        HumanCreator humanCreator = new HumanCreator();
+        University university = createUniversity("NTU", 4, humanCreator.createTypicalHuman());
+
+        return university;
+    }
+}
