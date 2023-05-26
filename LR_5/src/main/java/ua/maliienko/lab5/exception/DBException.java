998a7532acd65ba3998b8c11d3c74aad6b20b0a2
Anastasiia Mal <nastymal2003@gmail.com>
2023-05-26 15:57:14 +0200

    merged LR_5 into main

---
+++ LR_5/src/main/java/ua/maliienko/lab5/exception/DBException.java
@@ -0,0 +1,7 @@
+package main.java.ua.maliienko.lab5.exception;
+
+public class DBException extends Exception {
+    public DBException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
